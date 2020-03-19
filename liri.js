d63fb391b270ce780f42da286c26a57b16c01988
Keith Buchanan <Kbuchanan77@yahoo.com>
2020-03-18 20:20:44 -0700

    fixed the pick function

--- liri.js
+++ liri.js
@@ -111,6 +111,22 @@ var doWhatItSays = function() {
   });
 };
 
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case "concert-this":
+      getMyBands(functionData);
+      break;
+    case "movie-this":
+      getMovie(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
+  }
+};
+
 var runThis = function(argOne, argTwo) {
   pick(argOne, argTwo);
 };
